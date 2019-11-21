fdd020521ac8ca08ad72b2b0b57d2481a540fdb1
NiceGraphi <fnaumann@posteo.de>
2019-11-21 03:00:31 +0100

    hidden Api Key and chekc for http code
    now api key is not visible  in the
    online repository anymore. Also buit in a check if http reponse code is 200 or not. If not the code is returned to help with debugging.

--- GetData/GetData.py
+++ GetData/GetData.py
@@ -3,42 +3,29 @@ import json
 
 
 def get_weather_date(id='3220804', days=1):
-    pass
+    file = open('GetData\\APIkey.txt', 'r')
+    API_key = file.read()    
+    file.close()
+    
     base_url = "http://api.openweathermap.org/data/2.5/forecast?"
-    API_key = "f6633771b45cd04203686b97ff8bfa08"    
     city_id = id
 
     # This is final url. This is concatenation of base_url, API_key and city_id
     Final_url = base_url + "appid=" + API_key + "&id=" + city_id
-    weather_data = requests.get(Final_url).json()
+    print('End Url:', Final_url)
+    
+    weather_data = requests.get(Final_url)
+    
+    if weather_data.status_code != 200:
+        print("HTTP request went wrong, Code:", weather_data.status_code)
+        return None
+    
+    weather_data = weather_data.json()
+    
     records = days * 8
 
     return weather_data['list'][:records]
 
+
 def kelvinToCelsuis(kelvin):
     return kelvin - 273.15
-
-
-
-# API_key = "f6633771b45cd04203686b97ff8bfa08"
- 
-# # This stores the url
-# base_url = "http://api.openweathermap.org/data/2.5/forecast?"
- 
-# # This will ask the user to enter city ID
-# city_id = '3220804'
-
-# # This is final url. This is concatenation of base_url, API_key and city_id
-# Final_url = base_url + "appid=" + API_key + "&id=" + city_id
-# weather_data = requests.get(Final_url).json()
-
-# print('Days',len(weather_data['list']))
-
-# for value in weather_data['list']:
-#     print('Temp:' ,value['main']['temp'],end=', ')
-#     print('Temp:' ,value['main']['humidity'],end=', ')
-#     print('Weather:', value['weather'][0]['description'])
-
-# # humidity 
-
-
--- WeatherFeedConsole.py
+++ WeatherFeedConsole.py
@@ -1,16 +1,17 @@
 import sys
-sys.path.append('C:\\Users\\Naumann\\Desktop\\Pyt\\GetData')
+sys.path.append('C:\\Floskönigreich\\GitHub-BoolPurist\\WeatherFeed\\GetData')
 
 import datetime
-
 
 import GetData
 
 current_time = datetime.datetime.now()
 
 
-weather = GetData.get_weather_date(days=2)
+weather = GetData.get_weather_date()
+
 print(f'Today\'s date {current_time.day}.{current_time.month}.{current_time.year}:', end='\n\n')
+
 for value in weather:
     print(f'At {current_time.hour:02}:{current_time.minute:02}:{current_time.second:02}')
     celsius = GetData.kelvinToCelsuis(value['main']['temp'])
